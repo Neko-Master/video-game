16d0befd3f8c9d6184856cd4f488a558aa348ed0
Maxi <ge93tog@mytum.de>
2021-11-08 12:24:01 -0400

    Added 3 players, custom exit button and resized the map + bg_img

--- test/tuto/08b_anim.py
+++ test/tuto/08b_anim.py
@@ -1,44 +1,92 @@
+import os.path
+
 import pygame
 import random
 import copy
 
+
 def lire_images():
     imageBank = {}
-    imageBank["perso"]=pygame.image.load("Images/perso.png").convert_alpha()
-    imageBank["perso"]= pygame.transform.scale(imageBank["perso"], (48, 48))
-    imageBank["balle"]=pygame.image.load("Images/balle.png").convert_alpha()
-    imageBank["fond"]=pygame.image.load("Images/background.jpg").convert_alpha()
-    imageBank["mur"]=pygame.image.load("Images/mur.png").convert_alpha()
-    imageBank["mur"]= pygame.transform.scale(imageBank["mur"], (64, 64))
+    imageBank["perso"] = pygame.image.load("Images/perso.png").convert_alpha()
+    imageBank["perso"] = pygame.transform.scale(imageBank["perso"], (48, 48))
+    imageBank["exit_button"] = pygame.image.load("Images/Animations/platformerGraphics_otherStyle/Tiles/signExit.png")
+    imageBank["exit_button"] = pygame.transform.scale(imageBank["exit_button"], (50, 50))
+    imageBank["balle"] = pygame.image.load("Images/balle.png").convert_alpha()
+    imageBank["fond"] = pygame.image.load(
+        "Images/Animations/platformerGraphics_otherStyle/bg_castle.png").convert_alpha()
+    imageBank["mur"] = pygame.image.load("Images/mur.png").convert_alpha()
+    imageBank["mur"] = pygame.transform.scale(imageBank["mur"], (64, 64))
 
     imageBank["flame"] = []
     for i in range(4):
-      imageBank["flame"].append(pygame.image.load("Images/Animations/flameBall_"+str(i)+".png").convert_alpha())
-
-    imageBank["mc"] ={}
-    imageBank["mc"]["droite"]=[]
+        imageBank["flame"].append(pygame.image.load("Images/Animations/flameBall_" + str(i) + ".png").convert_alpha())
+    playerSize = 48
+    playerHeight = 64.59
+    imageBank["mc"] = {}
+    imageBank["mc"]["droite"] = []
     for i in range(3):
-      image = pygame.image.load("Images/Animations/mc-right-"+str(i)+".png").convert_alpha()
-      image = pygame.transform.scale(image, (48, 48))
-      imageBank["mc"]["droite"].append(image)
+        image = pygame.image.load("Images/Animations/mc-right-" + str(i) + ".png").convert_alpha()
+        image = pygame.transform.scale(image, (playerSize, playerSize))
+        imageBank["mc"]["droite"].append(image)
 
-    imageBank["mc"]["gauche"]=[]
+    imageBank["mc"]["gauche"] = []
     for i in range(3):
-      image = pygame.image.load("Images/Animations/mc-left-"+str(i)+".png").convert_alpha()
-      image = pygame.transform.scale(image, (48, 48))
-      imageBank["mc"]["gauche"].append(image)
+        image = pygame.image.load("Images/Animations/mc-left-" + str(i) + ".png").convert_alpha()
+        image = pygame.transform.scale(image, (playerSize, playerSize))
+        imageBank["mc"]["gauche"].append(image)
 
-    imageBank["mc"]["haut"]=[]
+    imageBank["mc"]["haut"] = []
     for i in range(3):
-      image = pygame.image.load("Images/Animations/mc-up-"+str(i)+".png").convert_alpha()
-      image = pygame.transform.scale(image, (48, 48))
-      imageBank["mc"]["haut"].append(image)
+        image = pygame.image.load("Images/Animations/mc-up-" + str(i) + ".png").convert_alpha()
+        image = pygame.transform.scale(image, (playerSize, playerSize))
+        imageBank["mc"]["haut"].append(image)
 
-    imageBank["mc"]["bas"]=[]
+    imageBank["mc"]["bas"] = []
     for i in range(3):
-      image = pygame.image.load("Images/Animations/mc-down-"+str(i)+".png").convert_alpha()
-      image = pygame.transform.scale(image, (48, 48))
-      imageBank["mc"]["bas"].append(image)
+        image = pygame.image.load("Images/Animations/mc-down-" + str(i) + ".png").convert_alpha()
+        image = pygame.transform.scale(image, (playerSize, playerSize))
+        imageBank["mc"]["bas"].append(image)
+
+        # create 3 players you can choose from
+        for i in range(3):
+            imageBank["player_" + str(i + 1)] = {}
+            # right
+            imageBank["player_" + str(i + 1)]["droite"] = []
+            for j in range(11):
+                if int(j) < 9:
+                    imageBank["player_" + str(i + 1)]["droite"].append(pygame.transform.scale(pygame.image.load(
+                        "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(i + 1) + "_walk/PNG/p" + str(
+                            i + 1) + "_walk0" + str(j + 1) + ".png").convert_alpha(), (playerSize, playerHeight)))
+                else:
+                    imageBank["player_" + str(i + 1)]["droite"].append(pygame.transform.scale(pygame.image.load(
+                        "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(i + 1) + "_walk/PNG/p" + str(
+                            i + 1) + "_walk" + str(j + 1) + ".png").convert_alpha(), (playerSize, playerHeight)))
+            # left
+            imageBank["player_" + str(i + 1)]["gauche"] = []
+            for j in range(11):
+                if int(j) < 9:
+                    imageBank["player_" + str(i + 1)]["gauche"].append(pygame.transform.scale(pygame.transform.flip(
+                        pygame.image.load(
+                        "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(i + 1) + "_walk/PNG/p" + str(
+                            i + 1) + "_walk0" + str(j + 1) + ".png").convert_alpha(),True, False), (playerSize, playerHeight)))
+                else:
+                    imageBank["player_" + str(i + 1)]["gauche"].append(pygame.transform.scale(pygame.transform.flip(
+                        pygame.image.load(
+                            "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(
+                                i + 1) + "_walk/PNG/p" + str(
+                                i + 1) + "_walk" + str(j + 1) + ".png"), True, False).convert_alpha(),
+                                                                                              (playerSize,
+                                                                                               playerHeight)))
+            # up
+            imageBank["player_" + str(i + 1)]["haut"] = []
+            imageBank["player_" + str(i + 1)]["haut"].append(pygame.transform.scale(pygame.image.load(
+                "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(i + 1) + "_jump.png").convert_alpha(),
+                                                                                    (playerSize, playerHeight)))
+            # down
+            imageBank["player_" + str(i + 1)]["bas"] = []
+            imageBank["player_" + str(i + 1)]["bas"].append(pygame.transform.scale(pygame.image.load(
+                "Images/Animations/platformerGraphics_otherStyle/Player/p" + str(i + 1) + "_duck.png").convert_alpha(),
+                                                                                   (playerSize, playerHeight)))
 
     return imageBank
 
@@ -48,35 +96,34 @@ def afficher_map(maMap, imageBank):
     nb_l = len(maMap)
     nb_c = len(maMap[0])
     for i in range(nb_l):
-        for j in range (nb_c):
-            if maMap[i][j]==1:
-                mur = ElementGraphique(imageBank["mur"], fenetre, x=64*j, y=64*i)
+        for j in range(nb_c):
+            if maMap[i][j] == 1:
+                mur = ElementGraphique(imageBank["mur"], fenetre, x=64 * j, y=64 * i)
                 mur.afficher()
+
 
 def collide_map(maMap, un_rect):
     # haut
-    irect = un_rect.y//64
-    jrect = int(un_rect.x/64)
+    irect = un_rect.y // 64
+    jrect = int(un_rect.x / 64)
 
-    if (maMap[irect][jrect] != 0) :
+    if (maMap[irect][jrect] != 0):
         return True
 
     # bas
-    irect = (un_rect.y+ un_rect.h)//64
-    jrect = int(un_rect.x/64)
+    irect = (un_rect.y + un_rect.h) // 64
+    jrect = int(un_rect.x / 64)
 
-    if (maMap[irect][jrect] != 0) :
+    if (maMap[irect][jrect] != 0):
         return True
 
-
     return False
-
 
 
 class ElementGraphique():
     def __init__(self, img, fen, x=0, y=0):
         self.image = img
-        self.rect= self.image.get_rect()
+        self.rect = self.image.get_rect()
         self.fenetre = fen
 
         # creation d'un rectangle pour positioner l'image du personnage
@@ -86,15 +133,34 @@ class ElementGraphique():
     def afficher(self):
         self.fenetre.blit(self.image, self.rect)
 
-    def contact(self,autre):
+    def contact(self, autre):
         if self.rect.colliderect(autre.rect):
             return True
         return False
 
+
+class Button(ElementGraphique):
+    def __init__(self, img, fen, x, y):
+        super().__init__(img, fen, x, y)
+        self.clicked = False
+
+    def afficher(self):
+        # mouseposition
+        pos = pygame.mouse.get_pos()
+        # Clicked on our button?
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        super().afficher()
+
+
 class ElementAnime(ElementGraphique):
     # images est un tableau des images de l'animation
     def __init__(self, images, fen, x=0, y=0):
-        super().__init__(images[0],fen, x, y)
+        super().__init__(images[0], fen, x, y)
         self.images = images
         self.delai = 10
         self.num_image = 0
@@ -103,7 +169,7 @@ class ElementAnime(ElementGraphique):
     def afficher(self):
         self.timer += 1
 
-        if self.timer > self.delai :
+        if self.timer > self.delai:
             self.num_image += 1
             if self.num_image >= len(self.images):
                 self.num_image = 0
@@ -113,6 +179,7 @@ class ElementAnime(ElementGraphique):
 
         super().afficher()
 
+
 class ElementAnimeDir(ElementAnime):
     # dico_images est le dictionnaire contenant toutes les animations par direction
     def __init__(self, dico_images, fen, x=0, y=0):
@@ -120,7 +187,7 @@ class ElementAnimeDir(ElementAnime):
         self.direction = "droite"
         self.old_dir = "droite"
 
-        super().__init__(self.dico_images[self.direction],fen, x, y)
+        super().__init__(self.dico_images[self.direction], fen, x, y)
 
     def afficher(self):
         if self.direction != self.old_dir:
@@ -133,7 +200,7 @@ class ElementAnimeDir(ElementAnime):
 
 class Joueur(ElementAnimeDir):
     def __init__(self, img, fen, x=0, y=0):
-        super().__init__(img,fen,x,y)
+        super().__init__(img, fen, x, y)
 
         self.vitesse = 5
 
@@ -143,25 +210,25 @@ class Joueur(ElementAnimeDir):
 
         new_rect = copy.deepcopy(self.rect)
 
-        if touches[pygame.K_RIGHT] :
-            self.direction="droite"
+        if touches[pygame.K_RIGHT]:
+            self.direction = "droite"
             new_rect.x += self.vitesse
 
-        if touches[pygame.K_LEFT] :
+        if touches[pygame.K_LEFT]:
             self.direction = "gauche"
             new_rect.x += -self.vitesse
 
-        if touches[pygame.K_UP] :
+        if touches[pygame.K_UP]:
             self.direction = "haut"
             new_rect.y += -self.vitesse
 
-        if touches[pygame.K_DOWN] :
+        if touches[pygame.K_DOWN]:
             self.direction = "bas"
             new_rect.y += self.vitesse
 
-        if collide_map(maMap,new_rect):
+        if collide_map(maMap, new_rect):
             print("Deplacement refusé")
-        else :
+        else:
             print("Deplacement accepté")
             self.rect = new_rect
 
@@ -169,22 +236,21 @@ class Joueur(ElementAnimeDir):
 class Balle(ElementAnime):
     def __init__(self, img, fen):
 
-        w , h = fen.get_size()
+        w, h = fen.get_size()
 
-        x = random.randint(0,w)
-        y = random.randint(0,h)
+        x = random.randint(0, w)
+        y = random.randint(0, h)
 
-        super().__init__(img,fen,x,y)
+        super().__init__(img, fen, x, y)
 
-        self.dx = random.randint(-5,5)
-        self.dy = random.randint(-5,5)
-
+        self.dx = random.randint(-5, 5)
+        self.dy = random.randint(-5, 5)
 
     def deplacer(self):
         self.rect.x += self.dx
         self.rect.y += self.dy
 
-        w , h = self.fenetre.get_size()
+        w, h = self.fenetre.get_size()
 
         # a gauche ou droite
         if self.rect.x < 0 or self.rect.x + self.rect.w > w:
@@ -195,21 +261,19 @@ class Balle(ElementAnime):
             self.dy = -self.dy
 
 
-
 # Initialisation de la bibliotheque pygame
 pygame.init()
 
-#creation de la fenetre
-largeur = 1040
-hauteur = 880
-fenetre=pygame.display.set_mode((largeur,hauteur))
+# creation de la fenetre
+largeur = 256 * 3
+hauteur = 256 * 3
+fenetre = pygame.display.set_mode((largeur, hauteur), pygame.NOFRAME)
 
 imageBank = lire_images()
 
 # lecture de l'image du perso
 
-perso = Joueur(imageBank["mc"], fenetre, x=60, y=80)
-
+perso = Joueur(imageBank["player_2"], fenetre, 80, 70)
 
 mes_balles = []
 for i in range(3):
@@ -217,7 +281,12 @@ for i in range(3):
     mes_balles.append(balle)
 
 # lecture de l'image du fond
-fond = ElementGraphique(imageBank["fond"],fenetre)
+fondarr = []
+for j in range(3):
+    for i in range(3):
+        fondarr.append(ElementGraphique(imageBank["fond"], fenetre, j * 256, 256 * i))
+
+exitButton = Button(imageBank["exit_button"], fenetre, (256 * 3) - 53, 3)
 
 ## Ajoutons un texte fixe dans la fenetre :
 # Choix de la police pour le texte
@@ -226,14 +295,14 @@ font = pygame.font.Font(None, 34)
 # Creation de l'image correspondant au texte
 texte = ElementGraphique(font.render('dot biten', True, (3, 45, 49)), fenetre, x=10, y=10)
 
-maMap = [[1,1,1,1,1,1,1,1,1,1],
-         [1,0,0,0,1,0,1,0,1,1],
-         [1,0,1,1,1,0,0,0,1,1],
-         [1,0,1,1,0,0,1,0,1,1],
-         [1,0,0,0,0,1,0,0,0,1],
-         [1,0,1,0,1,1,0,1,0,1],
-         [1,0,1,0,1,1,0,1,0,1],
-         [1,1,1,1,1,1,0,1,0,1],
+maMap = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 1, 0, 1, 0, 1, 1],
+         [1, 0, 1, 1, 1, 0, 0, 0, 1, 1],
+         [1, 0, 1, 1, 0, 0, 1, 0, 1, 1],
+         [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+         [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+         [1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+         [1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
          ]
 
 # servira a regler l'horloge du jeu
@@ -242,24 +311,23 @@ horloge = pygame.time.Clock()
 # la boucle dont on veut sortir :
 #   - en appuyant sur ESCAPE
 #   - en cliquant sur le bouton de fermeture
-i=1;
-continuer=1
+i = 1;
+continuer = 1
 while continuer:
 
     # fixons le nombre max de frames / secondes
     horloge.tick(30)
 
-    i=i+1
-    #print (i)
-
+    i = i + 1
+    # print (i)
 
     # on recupere l'etat du clavier
     touches = pygame.key.get_pressed();
 
     # si la touche ESC est enfoncee, on sortira
     # au debut du prochain tour de boucle
-    if touches[pygame.K_ESCAPE] :
-        continuer=0
+    if touches[pygame.K_ESCAPE] or exitButton.clicked == 1:
+        continuer = 0
 
     perso.deplacer()
 
@@ -288,14 +356,13 @@ while continuer:
     '''
 
     # Affichage du fond
-    fond.afficher()
-
+    for fonds in fondarr:
+        fonds.afficher()
 
     afficher_map(maMap, imageBank)
 
     # Affichage Perso
     perso.afficher()
-
 
     for e in mes_balles:
         e.afficher()
@@ -303,6 +370,7 @@ while continuer:
     # Affichage du Texte
     texte.afficher()
 
+    exitButton.afficher()
     # rafraichissement
     pygame.display.flip()
 
@@ -310,9 +378,9 @@ while continuer:
     # au debut du prochain tour de boucle
     # Pour cela, on parcours la liste des evenements
     # et on cherche un QUIT...
-    for event in pygame.event.get():   # parcours de la liste des evenements recus
-        if event.type == pygame.QUIT:     #Si un de ces evenements est de type QUIT
-            continuer = 0	   # On arrete la boucle
+    for event in pygame.event.get():  # parcours de la liste des evenements recus
+        if event.type == pygame.QUIT:  # Si un de ces evenements est de type QUIT
+            continuer = 0  # On arrete la boucle
 
 # fin du programme principal...
 pygame.quit()
