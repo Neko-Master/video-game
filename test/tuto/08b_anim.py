e8f94317ae9c36430f062d58ab9ce96ab903d4c6
Maxi <ge93tog@mytum.de>
2021-11-10 13:44:41 -0400

    added backgroundmusic to menu and game

--- test/tuto/08b_anim.py
+++ test/tuto/08b_anim.py
@@ -3,7 +3,6 @@ import os.path
 import pygame
 import random
 import copy
-import sys
 
 
 def lire_images():
@@ -96,6 +95,14 @@ def lire_images():
                                                                                    (playerSize, playerHeight)))
 
     return imageBank
+
+
+def lire_sounds():
+    soundBank = {}
+    soundBank["menu_music"] = pygame.mixer.Sound("Sounds/Menumusic.wav")
+    soundBank["menu_music"].set_volume(0.25)
+
+    return soundBank
 
 
 def afficher_map(maMap, imageBank):
@@ -275,15 +282,15 @@ def createTimerElements(fenetre, time):
         a = a + 33
 
 
-# Initialisation de la bibliotheque pygame
-pygame.init()
-
+pygame.init() # Initialisation de la bibliotheque pygame
+pygame.mixer.init() # initialize for sound
 # creation de la fenetre
 largeur = 256 * 3
 hauteur = 256 * 3
 fenetre = pygame.display.set_mode((largeur, hauteur), pygame.NOFRAME)
 
 imageBank = lire_images()
+soundBank = lire_sounds()
 
 # lecture de l'image du perso
 
@@ -299,7 +306,7 @@ fondarr = []
 for j in range(3):
     for i in range(3):
         fondarr.append(ElementGraphique(imageBank["fond"], fenetre, j * 256, 256 * i))
-
+#Create the buttons for the game
 playerButtons = []
 for i in range(3):
     playerButtons.append(Button(pygame.image.load(
@@ -307,7 +314,6 @@ for i in range(3):
             i + 1) + "_walk01.png"),
         fenetre, 100 * (i + 1), 256 / 2))
 playerButtons.append(Button(pygame.image.load("Images/Animations/mc-right-0.png"), fenetre, 400, 256 / 2))
-
 ingameExitButton = Button(imageBank["exit_button"], fenetre, (256 * 3) - 53, 3)
 menuStartButton = Button(imageBank["start_button"], fenetre, 256 / 2, 256)
 menuQuitButton = Button(imageBank["quit_button"], fenetre, 256 * 2, 256)
@@ -315,7 +321,7 @@ endScreenStartButton = Button(imageBank["start_button"], fenetre, 256 / 2, 256)
 endScreenQuitButton = Button(imageBank["quit_button"], fenetre, 256 * 2, 256)
 # Choix de la police pour le texte
 font = pygame.font.Font(None, 34)
-# Creation de l'image correspondant au texte
+# Text to display
 texte = ElementGraphique(font.render('The platformer of Maximilian Amougou and Tony Mardivirin', True, (3, 45, 49)),
                          fenetre, x=50, y=200)
 textePlayerMenu = ElementGraphique(font.render('Choose Player by clicking on him:', True, (3, 45, 49)), fenetre, x=100,
@@ -323,7 +329,9 @@ textePlayerMenu = ElementGraphique(font.render('Choose Player by clicking on him
 pauseText = ElementGraphique(
     font.render("Quit game? Press 'Y'es to end it all or 'N'o to resume the fun", True, (3, 45, 49)), fenetre, x=75,
     y=100)
+endScreenMessage = ElementGraphique(font.render("Try again?", True, (3, 45, 49)), fenetre, x=256, y=256)
 
+#draw this over screen to make it blurry
 blurryScreenImg = pygame.Surface((fenetre.get_size()))
 blurryScreenImg.fill((77, 77, 77))
 blurryScreenImg.set_alpha(111)
@@ -341,23 +349,29 @@ maMap = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 # servira a regler l'horloge du jeu
 horloge = pygame.time.Clock()
 
-# la boucle dont on veut sortir :
-#   - en appuyant sur ESCAPE
-#   - en cliquant sur le bouton de fermeture
+
+#hella variables for the game xD
 i = 1;
 continuer = 1
+# the menus
 main_menu = True
 player_selection_menu = False
 game_paused = False
 end_screen = False
 display_blurryScreen = False
 defaultPlayer = 1
+#timer stuff
 playtimePerLvl = 300  # time in seconds
 timeConst = playtimePerLvl
-endScreenMessage = ElementGraphique(font.render("Try again?", True, (3, 45, 49)), fenetre, x=256, y=256)
-start_ticks = pygame.time.get_ticks()
 secondsPassed = 0
 timerBuffer = 0
+#music stuff
+soundBank["menu_music"].play(10)
+lvlMusicPlaying=False
+pygame.mixer.music.load("Sounds/Backgroundmusic.ogg") #has to be done like this so you can puase/unpause
+pygame.mixer.music.set_volume(0.25)
+#start point of timer
+start_ticks = pygame.time.get_ticks()
 while continuer:
     # fixons le nombre max de frames / secondes
     horloge.tick(30)
@@ -386,6 +400,7 @@ while continuer:
         ingameExitButton.afficher()
         if ingameExitButton.clicked:
             continuer = 0
+        #generate the clickable players
         for i in range(4):
             playerButtons[i].afficher()
             if playerButtons[i].clicked:
@@ -398,23 +413,28 @@ while continuer:
         pygame.display.flip()
 
     elif game_paused:
+        #stop the timer while game is paused
         timerBuffer = secondsPassed
         if not display_blurryScreen:
             blurrScreen.afficher()
             display_blurryScreen = True
+        pygame.mixer.music.pause()
         pauseText.afficher()
         pygame.display.flip()
+        # another cool way to use buttons
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_y:
                     continuer = 0
                     break
                 if event.key == pygame.K_n:
+                    pygame.mixer.music.unpause()
                     game_paused = False
                     display_blurryScreen = False
                     start_ticks = pygame.time.get_ticks()
 
     elif end_screen:
+        pygame.mixer.music.stop()
         touches = pygame.key.get_pressed()
         if touches[pygame.K_ESCAPE]:
             continuer = 0
@@ -422,12 +442,14 @@ while continuer:
             fonds.afficher()
         if endScreenQuitButton.clicked:
             continuer = 0
-        if endScreenStartButton.clicked:  # reset time and stats here
+        # reset time and stats here to restart the game
+        if endScreenStartButton.clicked:
             end_screen = False
             playtimePerLvl = timeConst
             start_ticks = pygame.time.get_ticks()
             secondsPassed = 0
             timerBuffer = 0
+            lvlMusicPlaying = False
 
         endScreenMessage.afficher()
         endScreenStartButton.afficher()
@@ -435,6 +457,10 @@ while continuer:
         pygame.display.flip()
 
     else:
+        soundBank["menu_music"].stop()
+        if not lvlMusicPlaying:
+            lvlMusicPlaying=True
+            pygame.mixer.music.play(10)
         # on recupere l'etat du clavier
         touches = pygame.key.get_pressed()
         # si la touche ESC est enfoncee, on sortira
@@ -492,16 +518,7 @@ while continuer:
         createTimerElements(fenetre, playtimePerLvl)
         ingameExitButton.afficher()
         # rafraichissement
-
         pygame.display.flip()
-
-    # Si on a clique sur le bouton de fermeture on sortira
-    # au debut du prochain tour de boucle
-    # Pour cela, on parcours la liste des evenements
-    # et on cherche un QUIT...
-    for event in pygame.event.get():  # parcours de la liste des evenements recus
-        if event.type == pygame.QUIT:  # Si un de ces evenements est de type QUIT
-            continuer = 0  # On arrete la boucle
 
 # fin du programme principal...
 pygame.quit()
